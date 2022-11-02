25560b1aa67c70eb243747745d9a0c7ff7f8114d
Sean <36829049+stran556@users.noreply.github.com>
2022-11-02 11:10:15 -0500

    Add files via upload

--- filesort/sort.py
+++ filesort/sort.py
@@ -5,23 +5,46 @@ import subprocess
 loc1 = "/mnt/c/Users/Sean/Desktop"
 loc2 = "/mnt/c/Users/Sean/Documents"
 
+
 cmd = 'ls -p ' + loc1 + ' | grep -v / > lsfile.txt'
-
+os.system(cmd)
 file = open("lsfile.txt", "r")
-print(file.read())
-
 file_dir = []
 
-for line in file:
 
-    line = line[:-1]
-    if line == "":
-        break
-    else:
-        add_file_dir(line)
+def transfer_file_dir():
+    for file_t in file_dir:
+        cmd3 = 'mv ' + loc1 + '/' + file_t + ' ' + loc2
+        os.system(cmd3)
 
 
 def add_file_dir(name):
-    file_type = name[name.rfind("."):]
-    # os.system(file_type + ' > output.txt')
+    file_type = name[name.rfind(".") + 1:]
+    found = False
+    for f_type in file_dir:
+        if f_type == file_type:
+            found = True
+            continue
+    if not found:
+        file_dir.append(file_type)
+
+        # directory is created in source if necessary
+        cmd2 = 'mkdir ' + loc1 + '/' + file_type
+        os.system(cmd2)
+
     print(file_type)
+    return file_type
+
+
+for line in file:
+    line = line[:-1]
+    file_type3 = add_file_dir(line)
+
+    # mv /mnt/c/Users/Sean/Desktop/file.pdf /mnt/c/Users/Sean/Desktop/pdf
+    cmd4 = 'mv ' + loc1 + '/' + '\'' + line + '\'' + ' ' + loc1 + '/' + file_type3
+    os.system(cmd4)
+    # directory is moved to destination
+
+transfer_file_dir()
+
+print(file_dir)

b35af702698047778c945790387572c9ece7abd0
Sean <36829049+stran556@users.noreply.github.com>
2022-11-04 00:01:06 -0500

    Update disperse.py

--- filesort/disperse.py
+++ filesort/disperse.py
@@ -4,11 +4,12 @@ import os
 import subprocess
 
 # Preset source location and destination of disperse
-loc1 = "Input source location"
-loc2 = "Input destination"
-loc3 = "Input file location"
+loc1 = "/mnt/c/Users/Sean/Documents"
+loc2 = "/mnt/c/Users/Sean/Desktop"
+loc3 = "/mnt/c/Users/Sean/Desktop/bash/scripts/filesort"
 
 
+# Disperse contents of directory to destination
 def disperse(d):
     cmd2 = 'ls -p ' + loc1 + '\/' + d + ' > ' + loc3 + '\/' + 'lsfile.txt'
     os.system(cmd2)
@@ -16,22 +17,27 @@ def disperse(d):
     for name2 in file2:
         cmd3 = 'mv ' + loc1 + '\/' + d + '\/' + '\'' + name2.strip() + '\'' + ' ' + loc2
         os.system(cmd3)
+
+    # Delete empty directory folder
     cmd4 = 'rmdir ' + loc1 + '\/' + d
     os.system(cmd4)
 
 
+# Retrieve all directories in source location
 cmd = 'ls -p ' + loc1 + ' | grep / | rev | cut -c 2- | rev > ' + loc3 + '\/' + 'lsfile.txt'
 os.system(cmd)
 file = open(loc3 + "/lsfile.txt", "r")
-dir = []
+direc = []
 for line in file:
-    dir.append(line)
+    direc.append(line)
 
+
+# Read user input until terminated
 directory = " "
 while directory != "":
     directory = input('')
     found = False
-    for name in dir:
+    for name in direc:
         if name.strip() == directory:
             found = True
     if found:
@@ -40,6 +46,6 @@ while directory != "":
         if directory != "":
             print(directory + ": Directory not found.")
         else:
-            print("File dispersal ended.")
+            print("File dispersal terminated.")
 
 os.system('> lsfile.txt')

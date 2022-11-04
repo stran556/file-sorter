25c49196c88bcc0d2610d3c1056d322bac672654
Sean <36829049+stran556@users.noreply.github.com>
2022-11-04 00:02:09 -0500

    Update sort.py

--- filesort/sort.py
+++ filesort/sort.py
@@ -6,17 +6,12 @@ loc1 = "input source here"
 loc2 = "input destination here"
 loc3 = "input file-sort location here"
 
-cmd = 'ls -p ' + loc1 + ' | grep -v / > ' + loc3 + '\/'  + 'lsfile.txt'
+
+# Identify all non-directory files
+cmd = 'ls -p ' + loc1 + ' | grep -v / > ' + loc3 + '\/' + 'lsfile.txt'
 os.system(cmd)
 file = open(loc3 + "/lsfile.txt", "r")
 file_dir = []
-
-
-# Transfers directories from source to destination after all files are sorted
-def transfer_file_dir():
-    for file_t in file_dir:
-        cmd3 = 'mv ' + loc1 + '/' + file_t + ' ' + loc2
-        os.system(cmd3)
 
 
 # Creates directories if none are found
@@ -38,6 +33,7 @@ def add_file_dir(name):
     return file_type
 
 
+# Transfers file to directory based on file type
 for line in file:
     line = line[:-1]
     file_type3 = add_file_dir(line)
@@ -47,4 +43,11 @@ for line in file:
     cmd4 = 'mv ' + loc1 + '/' + '\'' + line + '\'' + ' ' + loc1 + '/' + file_type3
     os.system(cmd4)
 
-transfer_file_dir()
+# Transfers directories from source to destination after all files are sorted
+for file_t in file_dir:
+    cmd3 = 'mv ' + loc1 + '/' + file_t + ' ' + loc2
+    os.system(cmd3)
+
+print("\nFile sort complete.")
+os.system('> lsfile.txt')
+
